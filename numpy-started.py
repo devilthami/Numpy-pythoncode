7433f101d46c7d31118178ee5bca612bcdcd33b5
P@rashmgr <96672535+devilthami@users.noreply.github.com>
2023-10-19 15:18:00 +0545

    Add files via upload

---
+++ numpy-started.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+#arr = numpy.array([1, 2, 3, 4, 5])
+
+# arr = np.array([1, 3, 6, 9])
+
+# Create a 0-D array with value 42
+# arr = np.array(42)
+# print(arr)
+# print(type(arr))
+
+#Create a 1-D array containing the values 1,2,3,4,5:
+# arr = np.array([1, 2, 3, 4, 5])
+
+#Create a 2-D array conteining two arrays with the vaues
+
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+#Create a 3-D array with two 2-D arrays, both containing two arrays with the values 1,2,3 and 4,5,6:
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(arr)
